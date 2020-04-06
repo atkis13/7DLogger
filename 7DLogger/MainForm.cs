1b13cb18c2bf802c411afafefd1a8c33fcf92910
atkis13 <kis_attila_1349@yahoo.com>
2020-04-06 21:21:51 +0300

    cleanup

--- 7DLogger/MainForm.cs
+++ 7DLogger/MainForm.cs
@@ -22,17 +22,11 @@ namespace _7DLogger
 
         private void loadToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            try
-            {
-                Form_methods.ReadPDFLog("done", out_done);
-                Form_methods.ReadPDFLog("toDo", out_todo);
-                pictureBox1.ImageLocation = Application.StartupPath + @"\\images\\img.jpg";
-            }
-            catch(Exception ex)
-            {
-                MessageBox.Show("no logs found, please create" + ex.Message);
-            }
+            pictureBox1.ImageLocation = Application.StartupPath + @"\\images\\img.jpg";            
+            Form_methods.ReadPDFLog("done", out_done);
+            Form_methods.ReadPDFLog("toDo", out_todo);
             
+           
         }
 
         private void add_done_Click(object sender, EventArgs e)
@@ -40,6 +34,7 @@ namespace _7DLogger
             Form_methods.CreateNewLog(date_done.Text, in_done.Text, "done");
             Form_methods.EditLog(date_done.Text, previous, in_done.Text);
             Form_methods.ReadPDFLog("done", out_done);
+            MessageBox.Show("Logged");
 
 
 
@@ -50,17 +45,20 @@ namespace _7DLogger
             Form_methods.CreateNewLog(date_todo.Text, in_todo.Text, "toDo");
             Form_methods.EditLog(date_todo.Text, previous, in_todo.Text);
             Form_methods.ReadPDFLog("toDo", out_todo);
+            MessageBox.Show("Logged");
 
         }
 
         private void selectPic_Click(object sender, EventArgs e)
         {
             Form_methods.selectImg(piclabel, pictureBox1);
+            piclabel.Visible = true;
         }
 
         private void MainForm_Load(object sender, EventArgs e)
         {
             pictureBox1.ImageLocation = Application.StartupPath + @"\\images\\noimage.jpg";
+            piclabel.Visible = false;
         }
 
         private void addPic_Click(object sender, EventArgs e)
@@ -68,11 +66,32 @@ namespace _7DLogger
             
             Form_methods.addImgPicbox(piclabel);
             pictureBox1.ImageLocation = Application.StartupPath + @"\\images\\img.jpg";
+            MessageBox.Show("Picture updated");
         }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            pic p = new pic();
+            if (pictureBox1.ImageLocation == Application.StartupPath + @"\\images\\noimage.jpg")
+            {
+                p.Picloc = Application.StartupPath + @"\\images\\noimage.jpg";
+            }
+            else
+            {
+                p.Picloc = Application.StartupPath + @"\\images\\img.jpg";
+            }
+            p.Show();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            About abt = new About();
+            abt.Show();
         }
     }
 }
